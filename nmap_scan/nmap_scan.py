3e3296a0f3f73e51064fe33128ef35ec23dacad7
Jon Spriggs <jon@sprig.gs>
2023-07-10 07:32:46 +0100

    Typo invoking nmap

--- nmap_scan/nmap_scan.py
+++ nmap_scan/nmap_scan.py
@@ -37,7 +37,7 @@ remove_old_hosts = parser.read_configuration_variable(
 remove_old_host_delay = parser.read_configuration_variable(
     'IPAM_REMOVE_OLD_HOST_DELAY', default_value='48h')
 
-logging.info(f"Version: 1.0.7 Author: Jon Spriggs jon@sprig.gs")
+logging.info(f"Version: 1.0.8 Author: Jon Spriggs jon@sprig.gs")
 
 if server is None or api_client is None or api_token is None or agent_code is None:
     logging.error(f"Missing required values. Halting.")
@@ -278,7 +278,7 @@ while True:
                 logging.debug("Scan Start")
                 nmap = nmap3.NmapHostDiscovery()
                 hosts_list = nmap.nmap_no_portscan(
-                    cidr, arg=f"--unprivileged -T5{nameservers}{exclude}")
+                    cidr, args=f"--unprivileged -T5{nameservers}{exclude}")
                 nmap_stats = {
                     'runtime': hosts_list['runtime'], 'stats': hosts_list['stats'], 'task_results': hosts_list['task_results']}
 
